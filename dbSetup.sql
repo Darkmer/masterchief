031948e006de1648b8d9cd83bbe694700b5080f7
Dan <danfelizardo@gmail.com>
2014-04-03 17:50:03 -0400

    Added index.html and the sql code

---
+++ dbSetup.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 31, 2014 at 04:01 PM
+-- Server version: 5.6.11
+-- PHP Version: 5.5.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `softwaredevproject`
+--
+CREATE DATABASE IF NOT EXISTS `softwaredevproject` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `softwaredevproject`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(25) NOT NULL AUTO_INCREMENT,
+  `username` varchar(65) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `salt`, `email`) VALUES
+(1, 'admin', '30b821b232925780c9ba25b2a0fc30ec14ee73edb3d6e3c5d77f39dc94a3f09e', 'd64b00e41973112202093a389fe4a3028e8a8d0d916d6c3ba8d21a75cdd85cf9', 'gulenak@gmail.com');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
